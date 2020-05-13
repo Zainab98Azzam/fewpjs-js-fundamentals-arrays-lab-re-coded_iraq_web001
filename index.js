01f57387fb11694bc0ade90a94d6736d1f36d539
Zainab Azzam <zainab_azzam@yahoo.com>
2020-05-13 03:28:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const append =["Milo", "Otis", "Garfield"]
-const prepend= ["Milo", "Otis", "Garfield"]
-const removeLast=["Milo", "Otis", "Garfield"]
-const removeFirst=["Milo", "Otis", "Garfield"]
-append.push("Odio")
-prepend.unshift("cat")
+const append =["Milo", "Otis", "Garfield"];
+const prepend= ["Milo", "Otis", "Garfield"];
+const removeLast=["Milo", "Otis", "Garfield"];
+const removeFirst=["Milo", "Otis", "Garfield"];
+append.push("Odio");
+prepend.unshift("cat");
+removeLast.pop();
+removeFirst.unshift();
